0bd707ea4722dfa4217c43bd5c1d64e66d1f9461
Gutclara <mariadelasclaras@gmail.com>
2021-04-30 18:03:10 +0000

    falta abundante

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,22 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+/*import rigoImage from "../../img/rigo-baby.jpg";*/
 
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container col-12 d-flex">
+			<div className="icono col-2">
+				<i className="far fa-clock"></i>
+			</div>
+			<div className="quinto col-2"></div>
+			<div className="cuarto col-2"></div>
+			<div className="tercero col-2"></div>
+			<div className="segundo col-2"></div>
+			<div className="primero col-2">
+				<h1>hola</h1>
+			</div>
 		</div>
 	);
 }
